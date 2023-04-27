0be0d16f1f48ac6882d61ad649e07c3415004943
Mohamad Khatib <116002861+mohamad1999khatib@users.noreply.github.com>
2023-04-27 16:45:02 +0300

    TEST:Send_Message

--- User/topology.h
+++ User/topology.h
@@ -25,8 +25,8 @@
 
 // Topology
 static uint16_t array[__N ][7] ={
-	{_H0BR4, 0, 0, 0, 0, 0, _mod2 | P1}, 								 // Module 1
-	{_H26R0, _mod1 | P6, 0,0, 0, 0, 0},					    			 // Module 2
+	{_H0BR4, 0, 0, 0, 0, 0, _mod2 | P5}, 								 // Module 1
+	{_H01R0,0, 0,0, 0,  _mod1 | P6, 0},					    			 // Module 2
 };
 
 // Configurations for duplex serial ports
@@ -37,12 +37,12 @@ static uint16_t array[__N ][7] ={
 	#define	_P3pol_normal	1
 	#define	_P4pol_normal	1
 	#define	_P5pol_normal	1
-	#define	_P6pol_normal	1
+	#define	_P6pol_reversed	1
 #endif
 
 #if ( _module == 2 )
-	#define	H26R0			1
-	#define	_P1pol_reversed	1
+	#define	H01R0			1
+	#define	_P1pol_normal	1
 	#define	_P2pol_normal	1
 	#define	_P3pol_normal	1
 	#define	_P4pol_normal	1

24cf950bd3cf2359d354213233d6cc1ca6385339
ADEL\adool <adoolfaki@gmail.com>
2024-08-03 17:30:20 +0300

    Final Release candidate ---> 3.6

--- User/topology.h
+++ User/topology.h
@@ -17,18 +17,16 @@
 /* Includes ------------------------------------------------------------------*/
 #include "stm32g0xx_hal.h"
 
-#define __N	3					// Number of array modules
+#define __N	2					// Number of array modules
 
 // Array modules
 #define _mod1	1<<3
 #define _mod2	2<<3
-#define _mod3	3<<3
 
 // Topology
 static uint16_t array[__N ][7] ={
-	{_H01R0, _mod2 | P1, 0, 0, 0, 0, 0}, 						// Module 1
-	{_H0BR4, _mod1 | P1, 0, 0, _mod3 | P4, 0, 0},				// Module 2
-	{_H0AR9, 0, 0, 0, _mod2 | P4, 0, 0},					    // Module 3
+	{_H0BR4, _mod2 | P1, 0, 0, 0, 0, 0}, 				// Module 1
+	{_H01R0, _mod1 | P1, 0, 0, 0, 0, 0},				// Module 2
 };
 
 // Configurations for duplex serial ports
@@ -45,17 +43,6 @@ static uint16_t array[__N ][7] ={
 #if ( _module == 2 )
 	#define	H0BR4			1
 	#define	_P1pol_reversed	1
-	#define	_P2pol_normal	1
-	#define	_P3pol_normal	1
-	#define	_P4pol_reversed	1
-	#define	_P5pol_normal	1
-	#define	_P6pol_normal	1
-
-#endif
-
-#if ( _module == 3 )
-	#define	H0AR9			1
-	#define	_P1pol_normal	1
 	#define	_P2pol_normal	1
 	#define	_P3pol_normal	1
 	#define	_P4pol_normal	1

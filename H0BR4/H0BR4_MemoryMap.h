69c6b1cbd294a432a4d905c72e0ab5a028a7a641
Abdulrhman_Battikh <abdulrhmanbattikh123@gmail.com>
2025-02-02 14:55:53 +0300

    Editing memory map addresses

--- H0BR4/H0BR4_MemoryMap.h
+++ H0BR4/H0BR4_MemoryMap.h
@@ -17,20 +17,18 @@
 #ifdef FLASH_SIZE
 #undef FLASH_SIZE
 #endif
-/* Memory map: - STM32G0B0
- - Application: 0x08000000 - 0x0807FFFF >> 512 KB
- - Read-only (RO): 0x08040000 -  >> 2 KB, used to store topology information
-  - Read-only (RO): 0x08040800 -  >> 2 KB, used to store Command Snippets
- - Emulated EEPROM: 0x0807B800 -  >> 8 KB, fits 1000 16-bit variables in 4 pages
+/* Memory map: - STM32G0B0 : 0x08000000 - 0x0807FFFF >> 512 KB
+ - Application: 0x08000000 - 0x0807A7FF >> 500 KB
+ - Topology Address (RO): 0x0807A800 - 0x0807AFFF >> 2 KB, used to store topology information
+ - Snippets Address (RO): 0x0807B000 - 0x0807B7FF >> 2 KB, used to store Command Snippet
+ - Emulated EEPROM Address: 0x0807B800 - 0x0807F800 >> 8 KB, fits 1000 16-bit variables in 8 pages
  */
 #define APP_START_ADDRESS  		((uint32_t)0x08000000) 
-#define TOPOLOGY_START_ADDRESS  ((uint32_t)0x08040000)      // topology is stored here
-#define SNIPPETS_START_ADDRESS  ((uint32_t)0x08040800) 		// Snippets are stored here
-#define EEPROM_START_ADDRESS  	((uint32_t)0x0807B800U)      // EE_Variables are stored gere
-#define FLASH_SIZE				((uint32_t)0x20000)			// All sizes in bytes
-#define SRAM_SIZE				((uint32_t)0x8000)
-#define TOPOLOGY_PAGE_NUM		256
-#define SNIPPETS_PAGE_NUM		258
+#define TOPOLOGY_START_ADDRESS  ((uint32_t)0x0807A800)      /* topology is stored here 		*/
+#define SNIPPETS_START_ADDRESS  ((uint32_t)0x0807B000) 		/* Snippet are stored here      */
+#define EEPROM_START_ADDRESS  	((uint32_t)0x0807B800)      /* EE_Variables are stored here */
+#define TOPOLOGY_PAGE_NUM		374
+#define SNIPPETS_PAGE_NUM		373
 
 #ifdef __cplusplus
 }

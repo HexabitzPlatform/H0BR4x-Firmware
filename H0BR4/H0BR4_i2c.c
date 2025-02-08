cc800e4010ab23a8aee1c49dee2a88067a47f008
Mahmood_Reda <mr4141705@gmail.com>
2025-02-08 18:19:29 +0300

    Updated I2C config to maintain frequency and added clarification comments.

--- H0BR4/H0BR4_i2c.c
+++ H0BR4/H0BR4_i2c.c
@@ -38,36 +38,32 @@ I2C_HandleTypeDef hi2c2;
 
 }
 
-void MX_I2C2_Init(void){
+ /* I2C2 init function */
+ void MX_I2C2_Init(void)
+ {
+ 	/* USER CODE BEGIN I2C2_Init 0 */
 
-	/* USER CODE BEGIN I2C2_Init 0 */
+ 	/* USER CODE END I2C2_Init 0 */
 
-	/* USER CODE END I2C2_Init 0 */
+ 	/* Initialize I2C2 peripheral */
+ 	hi2c2.Instance = I2C2;
+ 	hi2c2.Init.Timing = 0x00C12166; // Fast mode (400 kHz)
+ 	hi2c2.Init.OwnAddress1 = 0; // No specific address required for master mode
+ 	hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT; // 7-bit addressing mode
+ 	hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE; // Disable dual address mode
+ 	hi2c2.Init.OwnAddress2 = 0; // Not used, set to 0
+ 	hi2c2.Init.OwnAddress2Masks = I2C_OA2_NOMASK; // No mask for second address
+ 	hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE; // Disable general call
+ 	hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE; // Disable clock stretching
+ 	HAL_I2C_Init(&hi2c2);
 
-	/* USER CODE BEGIN I2C2_Init 1 */
+ 	/** Configure Analogue filter */
+ 	HAL_I2CEx_ConfigAnalogFilter(&hi2c2, I2C_ANALOGFILTER_ENABLE); // Enable analog filter
 
-	/* USER CODE END I2C2_Init 1 */
-	hi2c2.Instance = I2C2;
-	hi2c2.Init.Timing =0x20303E5D;
-	hi2c2.Init.OwnAddress1 =0;
-	hi2c2.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-	hi2c2.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-	hi2c2.Init.OwnAddress2 =0;
-	hi2c2.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
-	hi2c2.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-	hi2c2.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-	HAL_I2C_Init(&hi2c2);
+ 	/** Configure Digital filter */
+ 	HAL_I2CEx_ConfigDigitalFilter(&hi2c2, 0); // Digital filter set to 0 (disabled)
 
-	/** Configure Analogue filter
-	 */
-	HAL_I2CEx_ConfigAnalogFilter(&hi2c2,I2C_ANALOGFILTER_ENABLE);
+ 	/* USER CODE BEGIN I2C2_Init 2 */
 
-	/** Configure Digital filter
-	 */
-	HAL_I2CEx_ConfigDigitalFilter(&hi2c2,0);
-
-	/* USER CODE BEGIN I2C2_Init 2 */
-
-	/* USER CODE END I2C2_Init 2 */
-
-}
+ 	/* USER CODE END I2C2_Init 2 */
+ }

3c35be682c82af4ba2a43868d6f0646b1685d094
Mahmood_Reda <mr4141705@gmail.com>
2025-02-11 16:04:07 +0300

    Updated USART1/2/3 clock source from PCLK1 to HSI

--- H0BR4/H0BR4_uart.c
+++ H0BR4/H0BR4_uart.c
@@ -199,7 +199,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart){
 	if(huart->Instance == USART1){
 #ifdef _Usart1
 		PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
-		PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK1;
+		PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_HSI;
 		HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit);
 
 		/* USART1 clock enable */
@@ -250,7 +250,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart){
 	else if(huart->Instance == USART2){
 #ifdef _Usart2
 		PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
-		PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+		PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_HSI;
 		HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit);
 
 		/* USART2 clock enable */
@@ -300,7 +300,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef *huart){
 	else if(huart->Instance == USART3){
 #ifdef _Usart3
 		PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART3;
-		PeriphClkInit.Usart3ClockSelection = RCC_USART3CLKSOURCE_PCLK1;
+		PeriphClkInit.Usart3ClockSelection = RCC_USART3CLKSOURCE_HSI;
 		HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit);
 
 		/* USART3 clock enable */

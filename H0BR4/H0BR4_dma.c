abb8bcac2003ac972e15154d596669f29070cad5
Abdulrhman_Battikh <abdulrhmanbattikh123@gmail.com>
2025-03-04 13:49:58 +0300

    Editing module's name in description

--- H0BR4/H0BR4_dma.c
+++ H0BR4/H0BR4_dma.c
@@ -2,7 +2,7 @@
  BitzOS (BOS) V0.3.6 - Copyright (C) 2017-2024 Hexabitz
  All rights reserved
 
- File Name     : H01R0_dma.c
+ File Name     : H0BR4_dma.c
  Description   : source file Contains Peripheral DMA setup .
 
  */

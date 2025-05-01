2cc3fcb721e7bb49741a2ae277c68acb2ca55279
Mahmood_Reda <mr4141705@gmail.com>
2025-05-01 15:44:57 +0300

    Fix bug in ADC polarity configuration.

--- H0BR4/H0BR4_inputs.h
+++ H0BR4/H0BR4_inputs.h
@@ -17,20 +17,20 @@
 /* Port-ADC Definitions */
 #define ADC_CH1_PIN   		GPIO_PIN_2
 #define ADC_CH2_PIN   		GPIO_PIN_3
-#define ADC_CH3_PIN   		GPIO_PIN_4
-#define ADC_CH4_PIN   		GPIO_PIN_5
+#define ADC_CH3_PIN   		GPIO_PIN_0
+#define ADC_CH4_PIN   		GPIO_PIN_1
 #define ADC12_PORT  		P2
-#define ADC34_PORT			P3
+#define ADC34_PORT			P1
 #define ADC12_GPIO_PORT  	GPIOA
 #define ADC34_GPIO_PORT		GPIOA
 #define ADC_CH1_USART   	USART2
 #define ADC_CH2_USART   	USART2
-#define ADC_CH3_USART   	USART3
-#define ADC_CH4_USART   	USART3
+#define ADC_CH3_USART   	USART4
+#define ADC_CH4_USART   	USART4
 #define ADC_CH1_CHANNEL   	ADC_CHANNEL_2
 #define ADC_CH2_CHANNEL   	ADC_CHANNEL_3
-#define ADC_CH3_CHANNEL   	ADC_CHANNEL_11
-#define ADC_CH4_CHANNEL   	ADC_CHANNEL_15
+#define ADC_CH3_CHANNEL   	ADC_CHANNEL_0
+#define ADC_CH4_CHANNEL   	ADC_CHANNEL_1
 
 /* Constant Macros */
 #define VREF_CAL            ((uint16_t *)((uint32_t)0x1FFF75AA))

d615e87f75e71df45f43d17d15afa176214946c2
Ahmad Abdulhai <@Aa52585258aA@>
2025-07-07 10:34:50 +0300

    Fixing bug in "USART6_AF" macro

--- H0BR4/H0BR4.h
+++ H0BR4/H0BR4.h
@@ -91,7 +91,7 @@
 #define	USART6_RX_PIN		GPIO_PIN_5
 #define	USART6_TX_PORT		GPIOA
 #define	USART6_RX_PORT		GPIOA
-#define	USART6_AF			GPIO_AF8_USART6
+#define	USART6_AF			GPIO_AF3_USART6
 
 /* I2C Pin Definition */
 #define I2C_SDA_PORT        GPIOA
